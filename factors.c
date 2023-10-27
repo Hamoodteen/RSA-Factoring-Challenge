827dc4d669cac9d87f5d1481a326c43f1ad27b9c
Hamoodteen <ahmed104567@outlook.com>
2023-10-27 23:52:29 +0200

    0

--- factors.c
+++ factors.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <gmp.h>
 
 /**
  * main - f
@@ -10,28 +11,37 @@
 */
 int main(int argc, char *argv[])
 {
-	uint64_t n, i, fact1 = 1, fact2 = 1;
+	mpz_t n, i, fact1, fact2;
 	char line[256];
 	FILE *f;
 
 	(void)argc;
+	mpz_inits(n, i, fact1, fact2, NULL);
 	f = fopen(argv[1], "r");
 	if (f == NULL)
 		return (1);
 	while (fgets(line, sizeof(line), f) != NULL)
 	{
-		n = atoll(line);
-		for (i = 2; i <= n / 2; i++)
+        mpz_set_str(n, line, 10);
+        mpz_set_ui(fact1, 1);
+        mpz_set_ui(fact2, 1);
+		for (mpz_set_ui(i, 2); mpz_cmp(i, n) <= 0; mpz_add_ui(i, i, 1))
 		{
-			if (n % i == 0)
-			{
-				fact1 = i;
-				fact2 = n / i;
-				break;
-			}
+            mpz_t rem;
+            mpz_init(rem);
+            mpz_mod(rem, n, i);
+            if (mpz_cmp_ui(rem, 0) == 0)
+            {
+                mpz_set(fact1, i);
+                mpz_tdiv_q(fact2, n, i);
+                mpz_clear(rem);
+                break;
+            }
+            mpz_clear(rem);
 		}
-		printf("%lu=%lu*%lu\n", n, fact2, fact1);
+		gmp_printf("%Zd=%Zd*%Zd\n", n, fact1, fact2);
 	}
+	mpz_clears(n, i, fact1, fact2, NULL);
 	fclose(f);
 	return (0);
 }
