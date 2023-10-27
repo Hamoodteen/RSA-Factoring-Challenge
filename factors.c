995020657d6611eee2092ff0c99a2a71f7e1b379
Hamoodteen <ahmed104567@outlook.com>
2023-10-27 23:16:26 +0200

    0

--- factors.c
+++ factors.c
@@ -9,6 +9,28 @@
 */
 int main(int argc, char *argv[])
 {
-	
+	size_t n, i, fact1 = 1, fact2 = 2;
+	char line[256];
+	FILE *f;
+
+	(void)argc;
+	f = fopen(argv[1], "r");
+	if (f == NULL)
+		return (1);
+	while (fgets(line, sizeof(line), f) != NULL)
+	{
+		n = atoi(line);
+		for (i = 1; i <= n; i++)
+		{
+			if (n % i == 0)
+			{
+				fact1 = i;
+				fact2 = n / i;
+				break;
+			}
+		}
+		printf("%s=%zu*%zu\n", line, fact1, fact2);
+	}
+	fclose(f);
 	return (0);
 }
